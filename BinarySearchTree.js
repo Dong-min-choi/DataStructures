db3b65f45251081299d9da941af75d29a42a3a6f
dmchoi <david5760@naver.com>
2021-07-16 16:23:22 +0900

    BinarySearchTree -최솟값, 최댓값 찾기, 특정값 찾기, 노드 삭제 메서드 추가

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -43,7 +43,7 @@ function BinarySearchTree() {
   //중위 순회
   this.inOrderTraverse = function (callback) {
     inOrderTraverseNode(root, callback);
-  }
+  };
 
   let inOrderTraverseNode = function (node, callback) {
 
@@ -57,7 +57,7 @@ function BinarySearchTree() {
   //전위 순회
   this.preOrderTraverse = function (callback) {
     preOrderTraverseNode(root, callback);
-  }
+  };
 
   let preOrderTraverseNode = function (node, callback) {
 
@@ -71,7 +71,7 @@ function BinarySearchTree() {
   //후위 순회
   this.postOrderTraverse = function (callback) {
     postOrderTraverseNode(root, callback);
-  }
+  };
 
   let postOrderTraverseNode = function (node, callback) {
 
@@ -81,6 +81,111 @@ function BinarySearchTree() {
       postOrderTraverseNode(node.right, callback);
       callback(node.key);
     }
+  };
+
+  this.min = function () {
+    return minNode(root);
+  };
+
+  let minNode = function (node) {
+    if (node) {
+      while (node && node.left !== null) {
+        node = node.left;
+      }
+
+      return node.key;
+    }
+    return null;
+  };
+
+  let findMinNode = function (node) {
+    while (node && node.left !== null) {
+      node = node.left;
+    }
+
+    return node;
+  };
+
+  this.max = function () {
+    return maxNode(root);
+  };
+
+  let maxNode = function (node) {
+    if (node) {
+      while (node && node.right !== null) {
+        node = node.right;
+      }
+
+      return node.key;
+    }
+    return null;
+  };
+
+  //특정 노드가 있는지 확인
+  this.search = function (key) {
+    return searchNode(root, key);
+  };
+
+  let searchNode = function (node, key) {
+
+    if (node === null) {
+      return false;
+
+    }
+    if (key < node.key) {
+      return searchNode(node.left, key);
+
+    } else if (key > node.key) {
+      return searchNode(node.right, key);
+
+    } else {
+      return true;
+
+    }
+  };
+
+  this.remove = function (key) {
+    root = removeNode(root, key);
+  };
+
+
+  let removeNode = function (node, key) {
+
+    if (node === null) {
+      return null;
+    }
+    if (key < node.key) {
+      node.left = removeNode(node.left, key);
+      return node;
+
+    } else if (key > node.key) {
+      node.right = removeNode(node.right, key);
+      return node;
+
+    } else { //key가 node.key와 같다.
+
+      //case 1 리프 노드
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      //case 2 자식이 하나뿐인 노드
+      if (node.left === null) { //오른쪽 자식만 존재
+        node = node.right;
+        return node;
+      } else if (node.right === null) { //왼쪽 자식만 존재
+        node = node.left;
+        return node;
+      }
+
+      //case 3 자식이 둘인 노드
+      let aux = findMinNode(node.right);
+      node.key = aux.key;
+      node.right = removeNode(node.right, aux.key);
+      return node;
+    }
+
   };
 
 }
