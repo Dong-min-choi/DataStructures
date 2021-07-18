009ec2d1dffca2135258cb1de0a08003e252b817
dmchoi <david5760@naver.com>
2021-07-18 23:02:09 +0900

    bfs 추가

--- Graph.js
+++ Graph.js
@@ -10,6 +10,7 @@ function Graph() {
   this.addEdge = function (v, w) { //무방향 그래프
     adjList.get(v).push(w);
     adjList.get(w).push(v);
+    console.log(adjList);
   };
 
   this.toString = function () {
@@ -23,5 +24,37 @@ function Graph() {
       s += '\n';
     }
     return s;
+  };
+
+  let initializeColor = function () {
+    let color = [];
+    for (let i = 0; i < vertices.length; i++) {
+      color[vertices[i]] = 'white';
+    }
+    return color;
+  };
+
+  this.bfs = function (v, callback) {
+
+    let color = initializeColor(),
+      queue = new Queue();
+    queue.enqueue(v);
+
+    while (!queue.isEmpty()) {
+      let u = queue.dequeue(),
+        neighbors = adjList.get(u);
+      color[u] = 'grey';
+      for (let i = 0; i < neighbors.length; i++) {
+        let w = neighbors[i];
+        if (color[w] === 'white') {
+          color[w] = 'grey';
+          queue.enqueue(w);
+        }
+      }
+      color[u] = 'black';
+      if (callback) {
+        callback(u);
+      }
+    }
   };
 }
