7a34ed5ffa1e3802f05875d9c41f7e4ae240f72b
dmchoi <david5760@naver.com>
2021-07-31 23:04:29 +0900

    ArrayList -insert, toString 메서드 구현

---
+++ ArrayList.js
@@ -0,0 +1,15 @@
+function ArrayList(){
+
+  let array = [];
+
+  this.insert = function(item){
+
+   array.push(item); 
+  };
+  
+  this.toString = function(){
+     
+    return array.join();
+  };
+
+}
