3677073c2e2999ec20415ee4d7fdfff3f316b97a
최동민 <dm.choi@logiall.com>
2024-04-10 23:20:11 +0900

    update

--- 백준 11654번 - 아스키코드.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString();
-let testCase = input;
-
-function solution(testCase){
-  console.log(testCase.charCodeAt());
-}
-
-solution(testCase);
